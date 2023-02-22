efb183aa9f54761658662a8711791610a596652c
Ankush Thakur <ankusht2307@gmail.com>
2023-02-23 00:14:52 +0530

    Refractored EditorContext types

--- src/shared/EditorContext.tsx
+++ src/shared/EditorContext.tsx
@@ -1,14 +1,26 @@
-import { createContext } from "react";
-
-export const EditorContext = createContext<any>(null);
+import { createContext, useState } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
+type EditorSettings = {
+  currentSelection: string;
+};
+
+type EditorContextType = {
+  editorContext: EditorSettings | null;
+  setEditorContext: React.Dispatch<React.SetStateAction<EditorSettings | null>>;
+};
+
+export const EditorContext = createContext<EditorContextType | null>(null);
+
 const Context = ({ children }: Props): JSX.Element => {
+  const [editorContext, setEditorContext] = useState<EditorSettings | null>(
+    null
+  );
   return (
-    <EditorContext.Provider value={"context"}>
+    <EditorContext.Provider value={{ editorContext, setEditorContext }}>
       {children}
     </EditorContext.Provider>
   );
