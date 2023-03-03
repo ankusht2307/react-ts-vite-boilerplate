ac1c954cb05fb6d0c7a8928328d4039fc62bf88d
Ankush Thakur <ankusht2307@gmail.com>
2023-03-04 01:06:51 +0530

    removed test case

--- src/__tests__/shared/EditorContext.test.tsx
+++ src/__tests__/shared/EditorContext.test.tsx
@@ -1,11 +1,8 @@
-import { renderHook } from "@testing-library/react";
-import { useContext } from "react";
 import { describe, it } from "vitest";
-import { EditorContext } from "../../shared/EditorContext";
 
 describe("sets editor context", () => {
   it("set initial value", () => {
-    const context = renderHook(useContext(EditorContext) as any);
-    console.log(context);
+    // const context = renderHook(useContext(EditorContext) as any);
+    // console.log(context);
   });
 });
